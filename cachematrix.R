ffbdb89e0b72a42903500296174b0c052723ecc6
curly51248 <terryheyn@gmail.com>
2015-01-25 13:38:02 -0600

    Update cachematrix.R
    Hopefully this is correct

--- cachematrix.R
+++ cachematrix.R
@@ -8,12 +8,31 @@
 ## Write a short comment describing this function
 ## makes the matrix ojbect
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverseMatrix <- function(solve) m <<- solve
+        getInverseMatrix <- function() m
+        list(set = set, get = get,
+             setInverseMatrix = setInverseMatrix,
+             getInverseMatrix = getInverseMatrix)
 }
-
 
 ## Write a short comment describing this function
 # computes the inverse of the matrix returned y makecachematrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+        m <- x$getInverseMatrix()
+        if(!is.null(m)) {
+                message("getting cached matrix")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setInverseMatrix(m)
+        m
 }
